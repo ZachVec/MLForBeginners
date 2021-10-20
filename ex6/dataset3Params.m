b90de3965c265e7ab4f6f5928b6775a2298a7d8d
vector <zijiebing@gmail.com>
2021-10-20 12:19:52 +0800

    pass hw: Support Vector Machines

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -22,13 +22,22 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
-
-
-
-
-
-
+C_alters = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];     % C alternatives
+sigma_alters = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]; % sigma alternatives
+error = 1;
+for i = 1 : 8
+    C_alter = C_alters(i);
+    for j = 1 : 8
+        sigma_alter = sigma_alters(j);
+        model = svmTrain(X, y, C_alter, @(x1, x2)gaussianKernel(x1, x2, sigma_alter));
+        predictions = svmPredict(model, Xval);
+        tmp_error = mean(double(predictions ~= yval));
+        if (tmp_error < error)
+            error = tmp_error;
+            C = C_alter;
+            sigma = sigma_alter;
+        end
+    end
+end
 % =========================================================================
-
 end
